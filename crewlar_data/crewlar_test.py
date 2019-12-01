bcb9b3600aa8dae938a7b0df9af0e591ef640256
mysoul <749468718@qq.com>
2019-12-01 19:43:14 +0800

    job data

--- crewlar_data/crewlar_test.py
+++ crewlar_data/crewlar_test.py
@@ -53,43 +53,56 @@ def request_url(url_string, cursor, db):
                 job = var['title']
             print("工作:", job)
 
-            sql = "INSERT INTO {0} (company, job, location, wages, pu_time, local_code) VALUES (%s, %s, %s, %s, %s, %s)".format("works")
-            if len(money):
-                val = (company[0].text, job, location[0].text, money[0].text, time[0].text, "090200")
-                cursor.execute(sql, val)
-                db.commit()
+            if company == "公司名":
+                pass
             else:
-                val = (company[0].text, job, location[0].text, "面议", time[0].text, "090200")
-                cursor.execute(sql, val)
-                db.commit()
-            # break
+                sql = "INSERT INTO {0} (company, job, location, wages, pu_time, local_code) VALUES (%s, %s, %s, %s, %s, %s)".format("jobs_table")
+                if len(money):
+                    val = (company[0].text, job, location[0].text, money[0].text, time[0].text, "090200")
+                    cursor.execute(sql, val)
+                    db.commit()
+                else:
+                    val = (company[0].text, job, location[0].text, "面议", time[0].text, "090200")
+                    cursor.execute(sql, val)
+                    db.commit()
 
-    print("完成")
+    print(url_string + "完成")
 
 
 def connect_db(db_tool, db_name):
     crewlar_db = db_tool.connect_sql_db(db_name)
     crewlar_db_cursor = crewlar_db.cursor()
 
-    table_name = "works"
+    table_name = "jobs_table"
     crewlar_db_cursor.execute("SHOW TABLES")
     has_table = is_table_exist(crewlar_db_cursor, table_name)
-    url_string = "https://search.51job.com/list/090200,000000,0000,00,9,99,%2B,2," \
-                 "1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare="
+    # url_string = "https://search.51job.com/list/090200,000000,0000,00,9,99,%2B,2," \
+    #              "1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare="
+    numbers = []
+    for i in range(1, 2000):
+        numbers.append(i)
+
     if has_table:
         print("存在表:", table_name)
         # 删除表
         # sql = "DROP TABLE IF EXISTS {}".format(table_name)
         # crewlar_db_cursor.execute(sql)
-
-        request_url(url_string, crewlar_db_cursor, crewlar_db)
+        for num in numbers:
+            url_str = "https://search.51job.com/list/090200,000000,0000,00,9,99,%2B,2," \
+                      "{0}.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=".format(str(num))
+            print(url_str)
+            request_url(url_str, crewlar_db_cursor, crewlar_db)
     else:
         crewlar_db_cursor.execute("CREATE TABLE {0} (id INT AUTO_INCREMENT PRIMARY KEY, company TEXT, job TEXT, location TEXT, wages TEXT, "
                                   "pu_time TEXT, local_code TEXT) CHARSET utf8".format(table_name))
         crewlar_db_cursor.execute("SHOW TABLES")
         if is_table_exist(crewlar_db_cursor, table_name):
             print("创建表{}成功".format(table_name))
-            request_url(url_string, crewlar_db_cursor, crewlar_db)
+            for num in numbers:
+                url_str = "https://search.51job.com/list/090200,000000,0000,00,9,99,%2B,2," \
+                          "{0}.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=".format(str(num))
+                print(url_str)
+                request_url(url_str, crewlar_db_cursor, crewlar_db)
 
 
 if __name__ == '__main__':
